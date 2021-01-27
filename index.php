1225350b1d49fd98f9ff267e110161eea407542a
Simon Bolduc <36117510+simbolduc@users.noreply.github.com>
2021-01-27 00:52:50 -0500

    Minor fixes

--- index.php
+++ index.php
@@ -28,21 +28,15 @@ if(is_array($match)) {
             require_once 'controllers/controller.php';
             require_once 'controllers/' . $exploded[0] . '.php';
             if (class_exists('\\controllers\\' . $exploded[0])) {
-                try {
-//                    $class = new ReflectionClass('\\controllers\\' . $exploded[0]);
-//                    $instance = $class->newInstanceArgs(array($router));
-                    $instance = getControllerClassInstance($exploded[0], $router, $db_conn);
-                    $response = call_user_func_array(array($instance, $exploded[1]), $match['params']);
-                    if(!$response)
-                        echo "Couldn't return demanded view";
-                } catch (ReflectionException $e) {
-                    echo $e->getMessage();
-                }
+                $instance = getControllerClassInstance($exploded[0], $router, $db_conn);
+                $response = call_user_func_array(array($instance, $exploded[1]), $match['params']) or die("Couldn't call specified method");
+                if(!$response)
+                    die("Couldn't return demanded view");
             } else {
-                echo "Specified controller class not found";
+                die("Specified controller class not found");
             }
         } else {
-            echo "Specified controller class not found";
+            die("Specified controller class not found");
         }
     }
     else {
@@ -50,11 +44,6 @@ if(is_array($match)) {
         $response = call_user_func_array(array($instance, 'view'), array($match['target'], $match['params']));
         if(!$response)
             echo "Couldn't return demanded view";
-//        $params = $match['params'];
-//        ob_start();
-//        require "views/pages/{$match['target']}.php";
-//        $page_content = ob_get_clean();
-//        require 'views/layouts/default.php';
     }
 } else {
     echo '404 page not found';
